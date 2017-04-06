d487a1d60fa844c59e15e7614de8840b3426f463
ezamlee <a.m.essam1992@gmail.com>
2017-04-06 07:38:05 +0200

    Added cookies check on all routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var mongoose=require("mongoose");
-
+var session = require('client-sessions');
 //Database Connections
 mongoose.connect("mongodb://127.0.0.1:27017/NodeProject");
 
@@ -32,56 +32,113 @@ notifications.find({},function(err,data){
     console.log(data);
 })
 
+app.use(session({
+  cookieName: 'session',
+  secret: 'abcdefghijk',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+
 
 
 //application setting
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+//middlewear
+
+
 // Routing
 app.use(express.static('public'));
 
 app.get("/register",function(req,resp){
-    resp.render("register",{title:"Register Me"})
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("register",{title:"Register Me"})
+    }
 })
 
 app.get("/add",function(req,resp){
-    resp.render("add_order",{title:"Make Order"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("add_order",{title:"Make Order"});
+    }
 })
 
 app.get("/allnotifications",function(req,resp){
-    resp.render("allNotifications",{title:"Notifications"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("allNotifications",{title:"Notifications"});
+    }
 })
 
 app.get("/home",function(req,resp){
-    resp.render("home",{title:"Home"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("home",{title:"Home"});
+    }
 })
 
 app.get("/",function(req,resp){
-    resp.render("login",{title:"Login"});
+    
+        resp.render("login",{title:"Login"});
+    
 })
 
 app.get("/profile",function(req,resp){
-    resp.render("profile",{title:"Profile"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("profile",{title:"Profile"});
+    }
 })
 
 app.get("/friends" , function(req,resp){
-    resp.render("friends",{title:"My Friends"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("friends",{title:"My Friends"});
+    }
 })
 app.get("/groups" , function(req,resp){
-    resp.render("groups",{title:"My Groups"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{    
+        resp.render("groups",{title:"My Groups"});
+    }
 })
 app.get("/orders" , function(req,resp){
-    resp.render("orders",{title:"Orders"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("orders",{title:"Orders"});
+    }
 })
 app.get("/details" , function(req,resp){
-    resp.render("details",{title:"Order Details"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("details",{title:"Order Details"});
+    }
 })
 app.get("/signup" , function(req,resp){
-    resp.render("signup",{title:"for test"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("signup",{title:"for test"});
+    }
 })
 app.get("/sign" , function(req,resp){
-    resp.render("sign",{title:"for test"});
+    if (!(req.session && req.session.user)) {
+        resp.redirect("/");
+    }else{
+        resp.render("sign",{title:"for test"});
+    }
 })
 
 
