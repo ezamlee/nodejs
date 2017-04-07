2aad152bfc38ec1e43902ec32c99cded22fa8f62
ezamlee <a.m.essam1992@gmail.com>
2017-04-07 11:51:40 +0200

    Latest updates

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,8 +1,25 @@
+var login = require("../models/logins");
+var users = require("../models/users");
+var orders = require("../models/orders");
+var notifications = require("../models/notifications");
 var express = require("express");
 var router = express.Router();
-router.get("/",function(req,resp){
-    resp.render("groups",{title:"My Groups"});
+var async = require("async");
+router.get("/", function (req, resp) {
+        resp.render("groups", { title: "My Groups",});
+})
+router.get("/list",function(req,resp){
+    var id = "ahmed@gmail.com";
+    users.find({"_id":id},(err,data) => {
+        console.log(data[0].groups);
+        resp.send(data[0].groups);
+    })
+})
+router.get("/member/:email",function(req,resp){
+    var id = "ahmed@gmail.com";
+    users.find({"_id":req.params.email},{"_id":1,"name":1,"img":1},(error,data) => {
+        resp.send(data);    
+    })
 })
 
 module.exports = router;
-
