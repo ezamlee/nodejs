2d10236f844c54d73241ab8a2058294abcddcd56
dohaes <dohaeseldin@gmail.com>
2017-04-19 15:07:07 +0200

    heroku v3

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,19 +7,19 @@ module.exports = {
 		'clientID' 		: '122153478327039', // your App ID
 		'clientSecret' 	: 'a2c887c63a4aa1f2973bc2b89de0605a', // your App Secret
 		'profileFields': [ 'emails'],
-		'callbackURL' 	: 'http://localhost:8090/auth/facebook/callback',
+		'callbackURL' 	: 'http://localhost:'+process.env.PORT+'/auth/facebook/callback',
 		'passReqToCallback' : true
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'your-consumer-key-here',
 		'consumerSecret' 	: 'your-client-secret-here',
-		'callbackURL' 		: 'http://localhost:8090/auth/twitter/callback'
+		'callbackURL' 		: 'http://localhost:'+process.env.PORT+'/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 			'clientID' 		: '865756533627-7b6dcc9esis3j3gr1lnuaviscqhkp5pu.apps.googleusercontent.com',
 			'clientSecret' 	: 'ixm4JS_1u5Va8gSEHev2bSN0',
-			'callbackURL' 	: 'http://localhost:8090/auth/google/callback'
+			'callbackURL' 	: 'http://localhost:'+process.env.PORT+'/auth/google/callback'
 		}
 };
